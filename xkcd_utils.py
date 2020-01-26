b07cc332b81a182513979b230ec670acb69b1498
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-01-26 22:33:22 +0800

    Store posted comics numbers
    Keep all posted numbers in one file.
    Delete first N numbers upon limit reach.
    Avoid repetition in posting.

--- xkcd_utils.py
+++ xkcd_utils.py
@@ -41,10 +41,42 @@ def fetch_comics(comics_number):
 
 def fetch_random_comics_for_group(group_name, vk_access_token):
     last_comics_number = get_last_xkcd_comics_number()
-    random_comics_number = random.randint(1, last_comics_number)
-    posted_comics_names = get_posted_comics_names(group_name, vk_access_token)
-    random_comics_name = get_comics_name(random_comics_number)
-    while random_comics_name in posted_comics_names:
-        random_comics_number = random.randint(1, last_comics_number)
-        random_comics_name = get_comics_name(random_comics_number)
+    random_comics_number = get_random_comics_number(last_comics_number)
+    save_comics_number_to_file(random_comics_number)
     return fetch_comics(random_comics_number)
+
+
+def get_random_comics_number(last_comics_number):
+    random_comics_number = random.randint(1, last_comics_number)
+    posted_comics_nums = get_posted_comics_nums()
+    while str(random_comics_number) in posted_comics_nums:
+        random_comics_number = random.randint(1, last_comics_number)
+    return random_comics_number
+
+def get_posted_comics_nums():
+    posted_comics_nums = []
+    posted_comics_list_limit = 2000
+    wipe_off_quantity = 500
+    try:
+        with open('posted_comics.txt', 'r') as posted_comics:
+            posted_comics_nums = posted_comics.read().split('\n')
+            if len(posted_comics_nums) >= posted_comics_list_limit:
+                remaining_nums = posted_comics_nums[wipe_off_quantity:]
+                posted_comics_nums = refresh_list_of_posted_comics(remaining_nums)
+    except FileNotFoundError:
+        pass   
+    return posted_comics_nums
+
+
+def save_comics_number_to_file(comics_number):
+    with open('published_comics.txt', 'a') as file:
+        file.write(str(comics_number) + '\n')
+
+
+def refresh_list_of_posted_comics(new_nums):
+    with open('posted_comics.txt', 'w') as posted_comics:
+        for new_num in new_nums:
+            posted_comics.write("%s\n" % new_num)
+    with open('posted_comics.txt', 'r') as posted_comics:
+        refreshed_posted_comics_nums = posted_comics.read().split('\n')
+    return refreshed_posted_comics_nums
