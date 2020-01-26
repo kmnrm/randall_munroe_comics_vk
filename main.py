adbbfb53ea7bdc9f68254dbd5a64dd4f355f5f56
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-01-26 23:08:16 +0800

    Erase temporary files in any case

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ from vk_utils import post_comics_on_groupwall
 def main():
     vk_access_token = os.getenv('VK_ACCESS_TOKEN')
     group_name = 'Комиксы Рэндела Манро'
-    comics_name = fetch_random_comics_for_group(group_name, vk_access_token)
-    post_comics_on_groupwall(comics_name, group_name, vk_access_token)
+    try:
+        comics_name = fetch_random_comics_for_group(group_name, vk_access_token)
+        post_comics_on_groupwall(comics_name, group_name, vk_access_token)
+    finally:
+        for file in os.listdir():
+            if file.endswith('.png'):
+                os.remove(file)
 
 if __name__ == '__main__':
     load_dotenv()
